c06c7b35d0201246882a11790a82226e360f215d
manikiran949 <manikiranbatchu6@gmail.com>
2026-06-04 10:55:09 +0530

    chore(api): update default backend url to localhost

--- flutter_app/lib/data/drape_api_client.dart
+++ flutter_app/lib/data/drape_api_client.dart
@@ -12,7 +12,7 @@ class DrapeApiClient {
 
   static const String defaultBaseUrl = String.fromEnvironment(
     'DRAPE_API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000',
+    defaultValue: 'http://127.0.0.1:8000',
   );
   static const String defaultApiKey = String.fromEnvironment('DRAPE_API_KEY', defaultValue: '');
   static const String defaultAuthToken = String.fromEnvironment('DRAPE_AUTH_TOKEN', defaultValue: '');
